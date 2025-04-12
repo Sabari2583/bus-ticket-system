b3713fa4db9e7e0ff283fbe7ea80b00b603a3ba1
Sabari2583 <sabari2583@gmail.com>
2025-04-12 21:40:45 +0530

    done

--- frontend/script.js
+++ frontend/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
 
             try {
-                const response = await fetch("http://localhost:3000/register", {
+                const response = await fetch("https://bus-ticket-system-k8p7.onrender.com", {
                     method: "POST",
                     headers: { "Content-Type": "application/json" },
                     body: JSON.stringify({ fullName, email, password })
